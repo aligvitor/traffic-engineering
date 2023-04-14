14838c793337908c2eeccc26f065e40bf76375ef
unknown <alig.vitor@gmail.com>
2023-04-13 21:38:29 -0300

    sobe

---
+++ LOS.py
@@ -0,0 +1,4 @@
+cruzamento = input ("Quantas aproximações tem o cruzamento?")
+cruzamento.lower()
+tipo_entrada = input("A aproximação do cruzamento é semaforizada ou não semaforizada?")
+tipo_entrada.lower()
