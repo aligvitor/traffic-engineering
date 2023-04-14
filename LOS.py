2bff2eeaa1a4d7ca7208f80f8b1ae178217131ad
unknown <alig.vitor@gmail.com>
2023-04-13 21:40:55 -0300

    sobe

--- LOS.py
+++ LOS.py
@@ -1,4 +1,10 @@
 cruzamento = input ("Quantas aproximações tem o cruzamento?")
 cruzamento.lower()
+
+for i in range (1,cruzamento+1)
+    letra += 
+
+print(f"Certo! Nomeie as {cruzamento} aproximações de A até {letra}")
+
 tipo_entrada = input("A aproximação do cruzamento é semaforizada ou não semaforizada?")
 tipo_entrada.lower()
